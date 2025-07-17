b255be181d11d8214c7232953ada1fbd99eecdee
Andy34G7 <andy34g7@gmail.com>
2025-07-17 17:09:40 +0530

    fix: Retrieval of team1 score has been fixed. Logic determining team's batting order is also improved.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hemanth-live_match_tracker
-A webpage that displays a list of ongoing cricket matches, given as project 1.
+A fron end webpage using HTML, CSS and JS that displays a list of ongoing cricket matches and their scores using the given API.
+
+Uses a free tier API key, so be frugal with tests and refreshes.
--- script.js
+++ script.js
@@ -51,16 +51,13 @@ function Matches_display(data) {
                     let team1Score = "";
                     let team2Score = "";
                     if (match.matchScore.team1Score && match.matchScore.team1Score.inngs1) {
-                        team1Score = match.matchScore.team1Score.inngs1.runs+"/" + match.matchScore.team1Score.inngs1.wickets + " (" + match.matchScore.team1Score.inngs1.overs + " overs)";
+                        team1Score = match.matchScore.team1Score.inngs1.runs + "/" + match.matchScore.team1Score.inngs1.wickets + " (" + match.matchScore.team1Score.inngs1.overs + " overs)";
                     }
                     if (match.matchScore.team2Score && match.matchScore.team2Score.inngs1){
-                        team2Score = match.matchScore.team2Score.inngs1.runs+"/" + match.matchScore.team2Score.inngs1.wickets + " (" + match.matchScore.team2Score.inngs1.overs + " overs)";
+                        team2Score = match.matchScore.team2Score.inngs1.runs + "/" + match.matchScore.team2Score.inngs1.wickets + " (" + match.matchScore.team2Score.inngs1.overs + " overs)";
                     }
-                    if (matchInfo.currBatTeamId === matchInfo.team1.teamId){
+                    if (match.matchScore.team1Score.inngs1.inningsId == 2){
                         scorestr = matchInfo.team2.teamSName + " " + team2Score + " | " + matchInfo.team1.teamSName + " " + team1Score;
-                    }
-                    else if (matchInfo.currBatTeamId === matchInfo.team2.teamId) {
-                        scorestr = matchInfo.team1.teamSName + " " + team1Score + " | " + matchInfo.team2.teamSName + " " + team2Score;
                     }
                     else {
                         scorestr = matchInfo.team1.teamSName + " " + team1Score + " | " + matchInfo.team2.teamSName + " " + team2Score;
@@ -73,7 +70,7 @@ function Matches_display(data) {
                     matchCard.appendChild(matchDetails);
                     
                     const venueInfo = document.createElement('p');
-                    venueInfo.textContent = "Venue: " + matchInfo.venueInfo.ground;
+                    venueInfo.textContent = "Venue: " + matchInfo.venueInfo.ground + ", " + matchInfo.venueInfo.city;
                     matchCard.appendChild(venueInfo);
 
                     seriesContainer.appendChild(matchCard);
@@ -88,6 +85,7 @@ function Matches_display(data) {
 async function fetch_display(){
     try {
         const data = await fetchLive();
+        console.log(data);
         Matches_display(data);
     }
     catch (error) {
@@ -95,42 +93,3 @@ async function fetch_display(){
     }
 }
 fetch_display();
-// fetch(settings.url, {method: settings.method, headers: settings.headers})
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Error fetching data: ' + response.statusText);
-//         }
-//         return response.json();
-//     })
-//     .then (data => {
-//         (data.typeMatches.forEach(typeMatch=> {
-//         console.log("Match type: ", typeMatch.matchType);
-//         typeMatch.seriesMatches.forEach(seriesMatch=>{
-//             if (seriesMatch.seriesAdWrapper && seriesMatch.seriesAdWrapper.matches){
-//                 const series = seriesMatch.seriesAdWrapper;
-//                 console.log("Series Name: ", series.seriesName);
-//                 series.matches.forEach(match=> {
-//                     console.log(match.matchInfo.matchDesc);
-//                     console.log("Match Format: ", match.matchInfo.matchFormat);
-//                     console.log("start date: ", match.matchInfo.startDate);
-//                     console.log("Toss: ", match.matchInfo.status);
-//                     console.log("Currently: ", match.matchInfo.state);
-//                     console.log(match.matchInfo.team1.teamSName, " vs ", match.matchInfo.team2.teamSName);
-//                     console.log("Happening at: ", match.matchInfo.venueInfo.ground,", ", match.matchInfo.venueInfo.city);
-//                     if (match.matchInfo.currBatTeamId == match.matchInfo.team1.teamId) {
-//                         console.log("Batting: ", match.matchInfo.team1.teamSName);
-//                     }
-//                     if (match.matchInfo.currBatTeamId == match.matchInfo.team2.teamId) {
-//                         console.log("Batting: ", match.matchInfo.team2.teamSName);
-//                     }
-//                     if (match.matchScore.team1Score){
-//                         console.log(match.matchInfo.team1.teamSName+"Score: ", match.matchScore.team1Score.inngs1.runs, "/", match.matchScore.team1Score.inngs1.wickets, " in ", match.matchScore.team1Score.inngs1.overs, " overs");
-//                     }
-//                     if (match.matchScore.team2Score) {
-//                         console.log(match.matchInfo.team2.teamSName + " Score: ", match.matchScore.team2Score.inngs1.runs, "/", match.matchScore.team2Score.inngs1.wickets, " in ", match.matchScore.team2Score.inngs1.overs, " overs");
-//                     }
-//                 })    
-//             }
-//         })
-//     }))
-//     });
